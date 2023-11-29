a97a7d67fd345a7dd6943c9349a8749ddcedcaeb
Jzurcc <johnreniel042206@gmail.com>
2023-11-30 05:25:41 +0800

    Added custom scrollbar

--- JS/articleScript.js
+++ JS/articleScript.js
@@ -1,53 +1,38 @@
 // Hide all articles except the first one on page load
-window.addEventListener('load', function() {
-    var articles = document.getElementsByClassName('article');
+window.addEventListener("load", function () {
+    var articles = document.getElementsByClassName("article");
     for (var i = 1; i < articles.length; i++) {
-        articles[i].style.display = 'none';
+        articles[i].style.display = "none";
     }
 });
 
 function toggleArticle(articleId) {
     // Hide all articles
-    var articles = document.getElementsByClassName('article');
+    var articles = document.getElementsByClassName("article");
     for (var i = 0; i < articles.length; i++) {
-        articles[i].style.display = 'none';
+        articles[i].style.display = "none";
     }
-    
+
     // Show the selected article
     var article = document.getElementById(articleId);
-    var links = document.getElementsByTagName('a');
+    var links = document.getElementsByTagName("a");
     for (var i = 0; i < links.length; i++) {
-        if (links[i].getAttribute('href') == `#${articleId}`) {
-            links[i].style.fontSize = '1.7rem';
+        if (links[i].getAttribute("href") == `#${articleId}`) {
+            links[i].style.fontSize = "1.7rem";
         } else {
-            links[i].style.fontSize = '1.5rem';
+            links[i].style.fontSize = "1.5rem";
         }
     }
-    article.style.display = 'block';
+    article.style.display = "block";
 }
 
 // Scrollbar
-const scrollbar = document.getElementById('scrollbar');
-let prevPercentage;
-let scrollPercentage;
-
-const handleOnScroll = e => {
-    const scrollDelta = e.deltaY;
-    const maxDelta = window.innerHeight / 2;
-    prevPercentage = scrollPercentage || "0";
-    const percentage = (scrollDelta / maxDelta) * -100;
-    const nextPercentageUnconstrained = parseFloat(prevPercentage) + percentage;
-    const nextPercentage = Math.max(Math.min(nextPercentageUnconstrained, 0), -100);
+window.addEventListener("scroll", function (event) {
+    let scrollY = this.scrollY;
+    let maxScrollY = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    scroll_percentage = (scrollY / maxScrollY) * 100
+    console.log(scroll_percentage)
     
-    scrollPercentage = nextPercentage;
-
-    scrollbar.animate(
-        {
-            transform: `translateY(${-nextPercentage}%`,
-        },
-        { duration: 900, fill: "both", easing: "ease-out" }
-    );
-};
-
-window.onwheel = e => handleOnScroll(e);
+    document.getElementById('scrollbar').style.top = `${scroll_percentage}%`;
+});
 
