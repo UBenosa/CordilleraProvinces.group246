3247e3dfc40cba6afc65e29a0173b0eb3f6482df
Jzurcc <johnreniel042206@gmail.com>
2023-12-04 01:51:29 +0800

    Added header elements hide/show button

--- JS/articleScript.js
+++ JS/articleScript.js
@@ -26,13 +26,47 @@ function toggleArticle(articleId) {
     }
 }
 
-
 // Scrollbar
 window.addEventListener("scroll", function (event) {
     let scrollY = this.scrollY;
-    let maxScrollY = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    let maxScrollY =
+        document.documentElement.scrollHeight -
+        document.documentElement.clientHeight;
     scroll_percentage = (scrollY / maxScrollY) * 100;
-    document.getElementById('scrollbar').style.top = `${scroll_percentage}%`;
-    
+    document.getElementById("scrollbar").style.top = `${scroll_percentage}%`;
 });
 
+is_shown = true;
+// Eye Image Show Icon
+function triggerOpacity() {
+    container = document.getElementsByClassName("parallax-container")[0];
+    eye = container.querySelectorAll("img")[1];
+    not_eye = container.querySelectorAll("*:not(img)");
+
+    if (is_shown) {
+        console.log(is_shown, 'yes')
+        eye.src = "../Images/Icons/hide.png";
+        not_eye.forEach((current_not_eye) => {
+            current_not_eye.animate(
+                {
+                    opacity: '0',
+                },
+                { duration: 600, fill: "both", easing: "ease" }
+            );
+        });
+        is_shown = false
+        
+    } else {
+        console.log(is_shown, 'no')
+        eye.src = "../Images/Icons/view.png";
+        not_eye.forEach((current_not_eye) => {
+            current_not_eye.animate(
+                {
+                    opacity: '1',
+                },
+                { duration: 600, fill: "both", easing: "ease" }
+            );
+        });
+        is_shown = true      
+    }
+}
